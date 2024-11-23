706a940952973a5efe1c2ee52c3fa7651cd0726b
Sean Mansell <22210423+arknano@users.noreply.github.com>
2024-11-23 15:29:41 +1030

    Initial project commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SYSTEMLESS = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+SYSTEMLESS.abilities = {
+  str: 'SYSTEMLESS.Ability.Str.long',
+  dex: 'SYSTEMLESS.Ability.Dex.long',
+  con: 'SYSTEMLESS.Ability.Con.long',
+  int: 'SYSTEMLESS.Ability.Int.long',
+  wis: 'SYSTEMLESS.Ability.Wis.long',
+  cha: 'SYSTEMLESS.Ability.Cha.long',
+};
+
+SYSTEMLESS.abilityAbbreviations = {
+  str: 'SYSTEMLESS.Ability.Str.abbr',
+  dex: 'SYSTEMLESS.Ability.Dex.abbr',
+  con: 'SYSTEMLESS.Ability.Con.abbr',
+  int: 'SYSTEMLESS.Ability.Int.abbr',
+  wis: 'SYSTEMLESS.Ability.Wis.abbr',
+  cha: 'SYSTEMLESS.Ability.Cha.abbr',
+};
